81f798f15acb4438defaf9f31674d57fb3dc7420
Julien Cretté <julien.crette@outlook.fr>
2022-11-23 21:28:55 +0100

    Big update on the logic part of the game

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
+let playerTurn = 0;
+let totalScores = {
+    playerOne: 0,
+    playerTwo: 0
+}
+
+const rollDice = () => {
+    let random = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    document.getElementById('dice-img').src = "./images/"+ random +".png";
+
+    let getScore = document.getElementById('current-' + playerTurn).innerText;
+    let newScore = Number(getScore) + random;
+
+    //random === 1 ? newScore = 0 : console.log('/');
+
+    let updateNewScore = document.getElementById('current-' + playerTurn).innerText = newScore;
+
+    let scoreTotal = 0;
+    playerTurn === 1 ? scoreTotal = totalScores.playerOne : scoreTotal = totalScores.playerTwo;
+    scoreTotal + updateNewScore >= 100 ? alert('Player '+ playerTurn +' wins !') : console.log('/');
+
+    newScore >= 100 ? setTimeout(() => { alert('Player '+ playerTurn +' wins !') }, 350) : updateNewScore;
+
+    // Revoir la fin rendu a 100 le calcule
+}
+
+const getCurrentPlayer = () => {
+    window.getComputedStyle(document.getElementById('turnP2')).display === 'none' ? playerTurn = 1 : playerTurn = 2;
+}
+
+const addedToTotal = (points) => {
+    points <= 0 ? alert('Impossible to add points. (Invalid value) Player ' + playerTurn)
+        : playerTurn === 1 ? totalScores.playerOne = totalScores.playerOne + points
+            : totalScores.playerTwo = totalScores.playerTwo + points;
+}
+
 window.addEventListener("DOMContentLoaded", (event) => {
+    getCurrentPlayer();
 
+    setTimeout(() => {
+        alert('Your turn to play Player 1 !');
+    }, 500);
 });
-
-let currentScoreP1 = 0;
-let totalScoreP1 = 0;
-
-let currentScoreP2 = 0;
-let totalScoreP2 = 0;
-
-let currentTurn = 1;
-
-function holdCurrentScore() {
-    if(currentTurn === 1) {
-        currentScore = currentScoreP1;
-    } else {
-        currentScore = currentScoreP2;
-    }
-    if(currentScore <= 0) {
-        alert('It is impossible to send your score. (0)');
-    } else {
-        if(currentTurn === 1) {
-            totalScoreP1 = totalScoreP1 + currentScore;
-            currentScoreP1 = 0;
-        } else {
-            totalScoreP2 = totalScoreP2 + currentScore;
-            currentScoreP2 = 0;
-        }
-    }
-}
-
-function checkCurrentPlayer() {
-    if(window.getComputedStyle(document.getElementById('turnP1')).display === 'none') {
-        currentTurn = 2;
-    } else {
-        currentTurn = 1;
-    }
-}
